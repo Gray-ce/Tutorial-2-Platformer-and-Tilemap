51ca08d9263c75cffe95e436cacd1492a6ec235c
Grace Ray <122415291+Gray-ce@users.noreply.github.com>
2023-03-20 14:50:03 -0400

    Level 1 completed

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,9 +10,12 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public Text score;
     public Text lives;
+    public GameObject Player;
     public GameObject winTextObject;
+    public GameObject loseTextObject;
     private int scoreValue = 0;
     private int livesValue = 3;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +23,9 @@ public class PlayerScript : MonoBehaviour
         rd2d = GetComponent<Rigidbody2D>();
         score.text = "Score:" +  scoreValue.ToString();
         winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
         lives.text = "Lives:" + livesValue.ToString();
+        transform.position = new Vector2(-1.22f , 0.01f);
     }
 
     // Update is called once per frame
@@ -30,7 +35,10 @@ public class PlayerScript : MonoBehaviour
         //float vertMovement = Input.GetAxis("Vertical"); nullified this code because the player kept moving upwards if the W key was held.
 
         rd2d.AddForce(new Vector2(hozMovement * speed, 0)); //, vertMovement * speed)); setting the vertical value to 0 constantly fixed this problem, as the player would now only be able to jump if touching the ground
+        
+
     }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Coin")
@@ -38,16 +46,30 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = "Score:" + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            lives.text = "Lives:" + livesValue.ToString();
+            if (scoreValue == 7)
+            {
+            int livesValue = 3;
+            lives.text = "Lives:" + livesValue.ToString();
+            transform.position  = new Vector2(38.8f , -0.28f);
+            }
         }
-         if (scoreValue >= 7)
+        if (scoreValue == 12)
         {
             winTextObject.SetActive(true);
         }
+        
         if (collision.collider.tag == "Enemy")
         {
             livesValue -= 1;
             lives.text = "Lives:" + livesValue.ToString();
             Destroy(collision.collider.gameObject);
+        }
+    
+        if (livesValue == 0)
+        {
+            loseTextObject.SetActive(true);
+            Destroy(Player);
         }
     }
      private void OnCollisionStay2D(Collision2D collision)
